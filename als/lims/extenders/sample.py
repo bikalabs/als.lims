5532608a12b7788e96703353c1b8c09cf26ec504
Alexander <alex@bikalabs.com>
2017-05-13 20:06:17 +0200

    removed and modified AR add fields (#1)

--- als/lims/extenders/sample.py
+++ als/lims/extenders/sample.py
@@ -1,21 +1,39 @@
 from archetypes.schemaextender.interfaces import IOrderableSchemaExtender
 from archetypes.schemaextender.interfaces import ISchemaModifier
 from bika.lims.interfaces import ISample
+from bika.lims.fields import ExtStringField
+from Products.Archetypes.public import *
+from bika.lims import bikaMessageFactory as _
 from zope.component import adapts
 from zope.interface import implements
 
+SampleConditionText =  ExtStringField(
+        'SampleConditionText',
+        widget=StringWidget(
+            label=_("Sample Condition"),
+            description= "",
+            visible={'view': 'visible',
+                     'edit': 'visible',
+                     'add': 'edit'},
+            render_own_label=True,
+            size=20
+        )
+    )
 
 class SampleSchemaExtender(object):
     adapts(ISample)
     implements(IOrderableSchemaExtender)
 
     fields = [
+        SampleConditionText,
     ]
 
     def __init__(self, context):
         self.context = context
 
     def getOrder(self, schematas):
+        index = schematas["default"].index("SamplePoint") + 1
+        schematas["default"].insert(index, "SampleConditionText")
         return schematas
 
     def getFields(self):
@@ -37,5 +55,7 @@ class SampleSchemaModifier(object):
         swe = self.context.bika_setup.getSamplingWorkflowEnabled()
         if swe:
             schema['SamplingDate'].required = False
+
+        schema.moveField("SampleConditionText", after="SamplePoint")
 
         return schema
